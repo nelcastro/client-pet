23eed22713da145a7b2d708cf9448918b1ecb504
Neilson <nelcastro1414@gmail.com>
2022-10-03 10:21:54 -0300

    implementa api de teste

--- src/main/java/br/com/petz/clientepet/ClientePetApplication.java
+++ src/main/java/br/com/petz/clientepet/ClientePetApplication.java
@@ -1,10 +1,20 @@
 package br.com.petz.clientepet;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ClientePetApplication {
+	
+	@GetMapping
+	public String geHomeTeste() {
+		
+		return 	"Cliente Pet - API Home";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientePetApplication.class, args);
