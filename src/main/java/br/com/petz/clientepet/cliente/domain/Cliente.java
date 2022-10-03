f7537db57f9c37573a1adae196154e501c1d1a24
Neilson <nelcastro1414@gmail.com>
2022-10-03 14:17:29 -0300

    ajusta mapeamento id uuid

--- src/main/java/br/com/petz/clientepet/cliente/domain/Cliente.java
+++ src/main/java/br/com/petz/clientepet/cliente/domain/Cliente.java
@@ -1,5 +1,75 @@
 package br.com.petz.clientepet.cliente.domain;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
 public class Cliente {
 
-}
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	@Column(name= "id", updatable=false, unique=true,nullable=false)
+	private UUID idCliente;
+	@NotBlank
+	private String nomeCompleto;
+	@NotBlank
+	@Email
+	private String email;
+	@NotBlank
+	private String celular;
+	private String telefone;
+	private Sexo sexo;
+	@NotNull
+	private LocalDate dataNascimento;
+	@CPF
+	private String cpf;
+	@NotNull
+	private Boolean aceitaTermos;
+	
+	
+	private LocalDateTime dataHoraDoCadastro;
+	private LocalDateTime dataHoraDaUltimaLatercao;
+	
+	public Cliente(@NotBlank String nomeCompleto, @NotBlank @Email String email, @NotBlank String celular,
+			String telefone, Sexo sexo, @NotNull LocalDate dataNascimento, @CPF String cpf,
+			@NotNull Boolean aceitaTermos, LocalDateTime dataHoraDoCadastro, LocalDateTime dataHoraDaUltimaLatercao) {
+		
+		this.nomeCompleto = nomeCompleto;
+		this.email = email;
+		this.celular = celular;
+		this.telefone = telefone;
+		this.sexo = sexo;
+		this.dataNascimento = dataNascimento;
+		this.cpf = cpf;
+		this.aceitaTermos = aceitaTermos;
+		this.dataHoraDoCadastro = dataHoraDoCadastro;
+		this.dataHoraDaUltimaLatercao = dataHoraDaUltimaLatercao;
+	}
+	
+	
+		
+	}
+	
+	
+	
+
