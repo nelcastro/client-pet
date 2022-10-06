ebeff52d7dc3fc28d7ff22344ba747ac40ccd96d
Neilson <nelcastro1414@gmail.com>
2022-10-06 10:56:16 -0300

    implementa clienteapplicationservice e corrige clienteAPI

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
@@ -1,5 +1,7 @@
 package br.com.petz.clientepet.cliente.application.api;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,5 +15,5 @@ public interface ClienteAPI {
 
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
-	ClienteResponse postCliente( @RequestBody ClientRequest clienteRequest);
+	ClienteResponse postCliente(@Valid @RequestBody ClientRequest clienteRequest);
 }
